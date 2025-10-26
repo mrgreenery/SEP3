03537feaa46c9bc30cf64ba6de900b9dbe00d3b4
Alex25073 <alexsolomca169@gmail.com>
2025-10-26 13:50:42 +0100

    added the Entities in C#

---
+++ Tier 2 Server C#/WebApplication1/WebApplication1/Entities/User.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace WebApplication1.Entities;
+
+public class User
+{
+    private long Id{ get; set; }
+    private string? Email{ get; set; }
+    private string? DisplayName{ get; set; }
+    private string? Password { get; set; }
+
+
+}
