05f195ad7355e8a334d0a1c93c96ecc09643e334
Samojak <123935006+Samojak@users.noreply.github.com>
2025-10-28 10:56:46 +0100

    added getter and setter to Task Entity(Tier 3)

--- Tier 3 Server JAVA/src/main/java/com/db/sep3/entities/Task.java
+++ Tier 3 Server JAVA/src/main/java/com/db/sep3/entities/Task.java
@@ -43,8 +43,88 @@ public class Task
   @Column(nullable=true)
   private Date endDate;
 
+  public Long getId()
+  {
+    return id;
+  }
 
+  public void setId(Long id)
+  {
+    this.id = id;
+  }
 
+  public String getTitle()
+  {
+    return title;
+  }
 
+  public void setTitle(String title)
+  {
+    this.title = title;
+  }
 
+  public String getDescription()
+  {
+    return description;
+  }
+
+  public void setDescription(String description)
+  {
+    this.description = description;
+  }
+
+  public String getStatus()
+  {
+    return status;
+  }
+
+  public void setStatus(String status)
+  {
+    this.status = status;
+  }
+
+  public Date getCreatedAt()
+  {
+    return createdAt;
+  }
+
+  public void setCreatedAt(Date createdAt)
+  {
+    this.createdAt = createdAt;
+  }
+
+  public User getCreatedBy()
+  {
+    return createdBy;
+  }
+
+  public void setCreatedBy(User createdBy)
+  {
+    this.createdBy = createdBy;
+  }
+
+  public Set<User> getAssignees()
+  {
+    return assignees;
+  }
+
+  public Date getStartDate()
+  {
+    return startDate;
+  }
+
+  public void setStartDate(Date startDate)
+  {
+    this.startDate = startDate;
+  }
+
+  public Date getEndDate()
+  {
+    return endDate;
+  }
+
+  public void setEndDate(Date endDate)
+  {
+    this.endDate = endDate;
+  }
 }
