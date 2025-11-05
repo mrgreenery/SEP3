22c2d53da959a067327091de3a50f79a0d385e67
samojak <samo.jakabovic11@gmail.com>
2025-11-05 13:44:53 +0100

    UserServiceImpl, cant test it bc proto is not created, but made all CRUD operations for user

---
+++ Tier 3 Server JAVA/src/main/java/com/db/sep3/gRPC/UserServiceImpl.java
@@ -0,0 +1,231 @@
+package com.db.sep3.gRPC;
+
+import com.db.sep3.DAO.UserRepository;
+import com.db.sep3.entities.User;
+import com.google.protobuf.Empty;
+import com.sep3.data.grpc.DataServiceGrpc;
+import com.sep3.data.grpc.IdRequest;
+import com.sep3.data.grpc.UserEntity;
+import com.sep3.data.grpc.UserList;
+import io.grpc.stub.StreamObserver;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class UserServiceImpl extends DataServiceGrpc.DataServiceImplBase {
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void createUSer(CreateUserREquest request, StreamObserver<UserEntity> responseObserver)
+    {
+        try{
+            System.out.println("=== Creating User ===");
+            System.out.println("Email: " + request.getUser().getEmail());
+            System.out.println("Display name: " + request.getUser().getDisplayName());
+            System.out.println("Password: *********** ");
+
+            //Create JPA entity
+            User user = new User();
+            user.setEmail(request.getUser().getEmail());
+            user.setDisplayName(request.getUser().getDisplayName());
+            user.setPassword(request.getUser().getPassword());
+
+            //save to database
+            User saved = userRepository.save(user);
+
+            System.out.println("User saved with ID: " + saved.getId());
+
+            //Convert to proto
+            UserEntity response = UserEntity.newBuilder()
+                    .setId(saved.getId())
+                    .setEmail(saved.getEmail())
+                    .setDisplayName(saved.getDisplayName())
+                    .setPassword(saved.getPassword())
+                    .build();
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+
+            System.out.println("=== User Created Successfully ===");
+        }catch (Exception e) {
+            System.out.println("Error creating user" + e.getMessage());
+            e.printStackTrace();
+            responseObserver.onError(e);
+        }
+    }
+
+    @Override
+    public void getUsersById(IdRequest request, StreamObserver<UserEntity> responseObserver)
+    {
+        try {
+            System.out.println("=== Getting Users by ID: " + request.getId() + " ===");
+
+            //Get user
+            User user = userRepository.findById(request.getId())
+                    .orElseThrow(() -> new jakarta.persistence.EntityNotFoundException
+                            ("User with id: " + request.getId() + " not found"));
+
+
+            //conver to proto
+            UserEntity response = convertToProto(user);
+
+            //respond
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+
+            System.out.println("=== User Found Successfully ===");
+        } catch (Exception e) {
+            System.out.println("Error getting user" + e.getMessage());
+            e.printStackTrace();
+            responseObserver.onError(e);
+        }
+
+
+    }
+
+    @Override
+    public void getUsersByEmail(EmailRequest request, StreamObserver<UserEntity> responseObserver)
+    {
+        try {
+            System.out.println("=== Getting Users by Email: " + request.getEmail() + " ===");
+
+            //Get user
+            User user = userRepository.findByEmail(request.getEmail())
+                    .orElseThrow(() -> new jakarta.persistence.EntityNotFoundException
+                            ("User with id: " + request.getEmail() + " not found"));
+
+
+            //conver to proto
+            UserEntity response = convertToProto(user);
+
+            //respond
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+
+            System.out.println("=== User Found Successfully ===");
+        } catch (Exception e) {
+            System.out.println("Error getting user" + e.getMessage());
+            e.printStackTrace();
+            responseObserver.onError(e);
+        }
+
+
+    }
+
+    @Override
+    public void getAllUsers(Empty request,StreamObserver<UserList>  responseObserver)
+    {
+        try{
+            System.out.println("=== Getting All Users ===");
+
+            //get Users
+            UserList.Builder builder = UserList.newBuilder();
+            userRepository.findAll().forEach(u -> builder.addUsers(convertToProto(u)));
+            UserList users = builder.build();
+
+            //send respond
+            responseObserver.onNext(users);
+            responseObserver.onCompleted();
+
+        }catch (Exception e)
+        {
+            System.out.println("Error getting users" + e.getMessage());
+            e.printStackTrace();
+            // if error send proper error
+            responseObserver.onError(
+                    io.grpc.Status.INTERNAL
+                            .withDescription("Failed to fetch users")
+                            .withCause(e)
+                            .asRuntimeException()
+            );
+        }
+    }
+
+    //this is more complicated bc of password, bc request dont have password (null)
+    //so i want to get the user, than update the email or display name
+    //and than save it without touching the password
+    @Override
+    public void updateUser(UpdateUserRequest request, StreamObserver<UserEntity> responseObserver) {
+        try {
+
+            //get user from request id
+            User user = userRepository.findById(request.getId())
+                    .orElseThrow(() -> new jakarta.persistence.EntityNotFoundException("User " + request.getId() + " not found"));
+
+
+            //check and update email and displayName
+            if (request.getUser().getEmail() != null && !request.getUser().getEmail().isEmpty())
+                user.setEmail(request.getUser().getEmail());
+
+            if (request.getUser().getDisplayName() != null && !request.getUser().getDisplayName().isEmpty())
+                user.setDisplayName(request.getUser().getDisplayName());
+
+            //save the user
+            User saved = userRepository.save(user);
+
+            //send respond
+            responseObserver.onNext(convertToProto(saved));
+            responseObserver.onCompleted();
+            System.out.println("=== User Updated Successfully ===");
+        } catch (Exception e) {
+            responseObserver.onError(io.grpc.Status.INTERNAL.withDescription("Failed to update user").withCause(e).asRuntimeException());
+        }
+    }
+
+
+    @Override
+    public void deleteUser(UserIdRequest request, StreamObserver<Empty> responseObserver) {
+        try {
+            System.out.println("=== Deleting User ===");
+
+            //check if the user exists
+            if (!userRepository.existsById(request.getId())) {
+                responseObserver.onError(
+                        io.grpc.Status.NOT_FOUND
+                                .withDescription("User " + request.getId() + " not found")
+                                .asRuntimeException()
+                );
+                return;
+            }
+
+            //if he exists delete it
+            userRepository.deleteById(request.getId());
+
+            //send respond of success
+            responseObserver.onNext(Empty.newBuilder().build());
+            responseObserver.onCompleted();
+
+        } catch (Exception e) {
+            responseObserver.onError(
+                    io.grpc.Status.INTERNAL
+                            .withDescription("Failed to delete user")
+                            .withCause(e)
+                            .asRuntimeException()
+            );
+        }
+    }
+
+
+
+
+
+
+
+
+    //util method for converting User to UserEntity
+    private UserEntity convertToProto(User user)
+    {
+        UserEntity userEntity = UserEntity.newBuilder()
+                .setId(user.getId())
+                .setEmail(user.getEmail())
+                .setDisplayName(user.getDisplayName())
+                .build();
+        return userEntity;
+    }
+
+
+
+}
