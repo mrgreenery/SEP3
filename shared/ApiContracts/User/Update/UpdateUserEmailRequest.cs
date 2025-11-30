5b09c8701ebf186b398534e656abcea0e59e84a1
mrgreenery <wilbergroen@gmail.com>
2025-11-30 13:14:29 +0100

    email format validation

--- shared/ApiContracts/User/Update/UpdateUserEmailRequest.cs
+++ shared/ApiContracts/User/Update/UpdateUserEmailRequest.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ApiContracts.User.Update;
 
 public record UpdateUserEmailRequest
 {
     public long Id { get; set; }
+    [EmailAddress (ErrorMessage = "Invalid email format")]
     public required string Email { get; set; }
 };
