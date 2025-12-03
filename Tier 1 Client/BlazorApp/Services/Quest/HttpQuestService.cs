f6ea9cf784a2a6e19e99240473d71e466ef456d4
mrgreenery <wilbergroen@gmail.com>
2025-12-03 14:19:53 +0100

    updated IQuestService.cs + HttpQuestService.cs so there is no mapping in createQuest method, but just plain request

--- Tier 1 Client/BlazorApp/Services/Quest/HttpQuestService.cs
+++ Tier 1 Client/BlazorApp/Services/Quest/HttpQuestService.cs
@@ -14,36 +14,16 @@ public class HttpQuestService : IQuestService
         this.client = client;
     }
     
-    public async Task<QuestDto> CreateQuestAsync(string title, 
-        string? description, QuestStatus status, long createdById, 
-        long? assigneeId, DateTime? startDate, DateTime? deadline, 
-        DateTime? finishedDate)
+    public async Task<QuestDto> CreateQuestAsync(CreateQuestRequest request)
     {
-        // Build a request body
-        CreateQuestRequest request = new CreateQuestRequest
-        {
-            Title = title,
-            Description = description,
-            Status = status,
-            CreatedById = createdById,
-            AssigneeId = assigneeId,
-            StartDate = startDate,
-            Deadline = deadline,
-            FinishedDate = finishedDate
-        };
-        
-        // Send a request and receive a response message
-        var respond = await client.PostAsJsonAsync(
-            "api/quests",  request);
-        
+        var respond = await client.PostAsJsonAsync("api/quests", request);
         respond.EnsureSuccessStatusCode();
-        
-        //Change response message into a QuestDto and return it
+    
         return await respond.Content.ReadFromJsonAsync<QuestDto>(
                    new JsonSerializerOptions{PropertyNameCaseInsensitive = true})
                ?? throw new Exception("Could not create quest");
     }
-    
+
     public async Task UpdateQuestAsync(long id, QuestDto questDto)
     {
         // Calling the Tier 2: PUT /api/quests/{id}
