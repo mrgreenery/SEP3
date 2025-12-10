d6508aededb102149a3a579247a8a943a59594c9
samojak <samo.jakabovic11@gmail.com>
2025-12-10 09:59:21 +0100

    Syncfusion lycense and not succesfull implementation of kanban board

--- Tier 1 Client/BlazorApp/Program.cs
+++ Tier 1 Client/BlazorApp/Program.cs
@@ -3,14 +3,19 @@ using BlazorApp.Services.Auth;
 using BlazorApp.Services.Quest;
 using BlazorApp.Services.User;
 using Microsoft.AspNetCore.Components.Authorization;
+using Syncfusion.Blazor;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JFaF5cXGRCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWH1eeXRdR2ZeV0d0WEBWYEg=");
+
 
 builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri("https://localhost:7009")
 });
+
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -19,13 +24,12 @@ builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, AuthProvider>();
 builder.Services.AddScoped<IUserService, HttpUserService>();
 builder.Services.AddScoped<IQuestService, HttpQuestService>();
-builder.Services.AddSingleton<QuestHubService>(); //adding signalR service
-
+builder.Services.AddScoped<QuestHubService>(); //adding signalR service
+builder.Services.AddSyncfusionBlazor();
 
 var app = builder.Build();
 
-var hubService = app.Services.GetRequiredService<QuestHubService>(); //connect to hub on start-up
-await hubService.ConnectAsync("https://localhost:7009/questhub");
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
